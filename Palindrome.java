120e3d4d06e350b6c722e62376c362c27a340ae8
Wolf Elkan <wolf.elkan@gmail.com>
2021-09-14 20:35:07 -0400

    UI Skeleton

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		Scanner user = new Scanner(System.in);
+		System.out.println("Enter a word or phrase:");
+		String input = user.nextLine();
+		System.out.print(input);
+		System.out.println(" might be a palindrome.");
+		System.out.println("I haven't actually written any code yet.");
+	}
+}
